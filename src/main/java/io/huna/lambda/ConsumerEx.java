8b09723b2f4bc8e9a963b5f01399e623658d49fa
hun-a <seunghun.chan@gmail.com>
2020-02-07 21:24:34 +0900

    Add a code for Consumer Functional Interface

---
+++ src/main/java/io/huna/lambda/ConsumerEx.java
@@ -0,0 +1,21 @@
+package io.huna.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerEx {
+
+    public static void main(String[] args) {
+       forEach(
+               Arrays.asList(1, 2, 3, 4, 5),
+               i -> System.out.println(i)
+       );
+    }
+
+    public static <T> void forEach(List<T> list, Consumer<T> c) {
+        for (T t: list) {
+            c.accept(t);
+        }
+    }
+}
