256328ba3ae5d9178866a37bac3f83c11599e152
hun-a <seunghun.chan@gmail.com>
2020-02-12 18:43:46 +0900

    Add a code for Supplier Functional Interface

---
+++ src/main/java/io/huna/lambda/SupplierEx.java
@@ -0,0 +1,27 @@
+package io.huna.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class SupplierEx {
+
+    public static void main(String[] args) {
+        Supplier<Integer> randomGenerator = () -> new Random().nextInt(100);
+        List<Integer> list = generateList(10, randomGenerator);
+
+        for (Integer i: list) {
+            System.out.println(i);
+        }
+    }
+
+    public static List<Integer> generateList(int size, Supplier<Integer> supplier) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            result.add(supplier.get());
+        }
+        return result;
+    }
+}
