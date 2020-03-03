8cee0d35b71cf48b23676f0c9b001710a3493e88
hun-a <seunghun.chan@gmail.com>
2020-03-03 18:02:05 +0900

    Add a Reduce example of the Stream API

---
+++ src/main/java/io/huna/stream/ReduceEx.java
@@ -0,0 +1,24 @@
+package io.huna.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReduceEx {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        System.out.println(
+                list.stream()
+                        .reduce(Integer::sum)
+                        .orElse(0)
+        );
+
+        System.out.println(
+                list.stream()
+                        .collect(Collectors.reducing(Integer::sum))
+                        .orElse(0)
+        );
+    }
+}
