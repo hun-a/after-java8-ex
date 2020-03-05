fb3d764902da815fdd65c7cfac51ebe3537bdfb1
huna <seunghun.chan@gmail.com>
2020-03-05 13:50:14 +0000

    Add the Limit and Findfirst example of the Stream API

---
+++ src/main/java/io/huna/stream/FindEx.java
@@ -0,0 +1,23 @@
+package io.huna.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindEx {
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("ABC", "DEF", "G", "HI", "J", "KL", "MNO");
+
+        list.stream()
+                .filter(s -> s.length == 1)
+                .limit(1)
+                .forEach(System.out::println);
+        System.out.println();
+        System.out.println(
+          list.stream()
+                  .filter(s -> s.length == 1)
+                  .findFirst()
+                  .get()
+        );
+    }
+}
